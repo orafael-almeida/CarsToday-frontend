618913634446f513dc89ee94e5d96ea0d07922e5
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-16 20:07:48 -0300

    fix: add footer to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "./sections/Header";
+import Footer from "./sections/Footer";
 
 export const metadata: Metadata = {
   title: "CarsToday - Buy and Sell Cars",
@@ -14,9 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased bg-slate-200 relative`}>
+      <body className={`antialiased bg-slate-200 relative h-screen flex flex-col justify-between`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
