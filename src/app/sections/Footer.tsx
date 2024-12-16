07287c8a01a54ebfef19a1c27e55fccbe62478f7
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-16 17:44:12 -0300

    feat: create footer component

---
+++ src/app/sections/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="w-full flex items-center justify-center h-16 bg-white drop-shadow mt-24">
+      <div className="flex w-full max-w-7xl items-center justify-between px-4 mx-auto">
+        <span className="text-sm text-gray-500 sm:text-center">
+          CarsToday © - 2024 . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
