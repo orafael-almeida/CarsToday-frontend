115176d6016dab54685c0d4ebf4c35c85298a518
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-16 20:08:34 -0300

    feat: create dynamic page when navigate with make and year params

---
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -0,0 +1,12 @@
+import CarsTable from "@/app/components/CarsTable";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col justify-center pt-32">
+      <CarsTable />
+    </div>
+  );
+};
+
+export default page;
