4c642454c779aca57578a4ed27b7aa3206fbdaf3
Rafael Almeida <almeidarp.dev@gmail.com>
2024-12-16 16:27:46 -0300

    feat: applies header navbar to the pages layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Header } from "./sections/Header";
 
 export const metadata: Metadata = {
   title: "CarsToday - Buy and Sell Cars",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`$antialiased`}>{children}</body>
+      <body className={`antialiased bg-slate-200`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
